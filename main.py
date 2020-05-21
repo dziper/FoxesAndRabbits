7b3ff8d19a0ec350c32d5f2068ffcaaaa4070990
dziper <dziper@me.com>
2020-05-21 14:20:10 -0700

    create sim window in pg

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello")
+import pygame as pg
+import random
+import copy
+
+
+res = 20
+w = 16
+h = 16
+
+pxw = w * res
+pxh = h * res
+
+size = pxw, pxh
+fps = 10
+
+pg.init()
+screen = pg.display.set_mode(size)
+pg.display.set_caption('Foxes and Rabbits')
+clock = pg.time.Clock()
+
+
+running = True
+while running:
+    screen.fill((0,0,0))
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN:
+            pass
