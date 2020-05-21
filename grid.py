dd24dab0520f9a4c32810ca74e957bde6efd265d
dziper <dziper@me.com>
2020-05-21 14:36:30 -0700

    create grid and fill with 0s

---
+++ grid.py
@@ -0,0 +1,19 @@
+
+class Grid:
+    size = (None, None)
+    grid = []
+
+    def __init__(self, size):
+        self.size = size
+        self.grid = self.getEmpty()
+
+    def getEmpty(self):
+        emptyGrid = [None]*self.size[0]
+        for i in range(len(emptyGrid)):
+            row = [None]*self.size[1]
+            emptyGrid[i] = row
+        return emptyGrid
+
+    def print(self):
+        for row in self.grid:
+            print(row)
--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import pygame as pg
 import random
 import copy
-
+import grid as g
 
 res = 20
 w = 16
 h = 16
+size = (w,h)
 
 pxw = w * res
 pxh = h * res
 
-size = pxw, pxh
+pxsize = pxw, pxh
 fps = 10
 
 pg.init()
-screen = pg.display.set_mode(size)
+screen = pg.display.set_mode(pxsize)
 pg.display.set_caption('Foxes and Rabbits')
 clock = pg.time.Clock()
+
+grid = g.Grid(size)
+grid.print()
 
 paused = False
 running = True
 while running:
     screen.fill((0,0,0))
     clock.tick(fps)
-    
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.KEYDOWN:
-            if event.key = pg.K_SPACE:
-                paused = !paused
+            if event.key == pg.K_ESCAPE:
+                running = False
+            if event.key == pg.K_SPACE:
+                paused = not paused
