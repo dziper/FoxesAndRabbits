21048c892a1656aa15ab6ae69fb5c831462b89ef
Nika Ziper <NIKA@Innas-MacBook-Air.local>
2020-06-02 10:37:09 -0700

    Created a predator clas within the animal class

--- fox.py
+++ fox.py
@@ -1,6 +1,5 @@
-import random
-import animal
-class Fox(animal.Animal):
+import predator
+class Fox(predator.Predator):
     MAX_AGE = 10
     NAME = "F"
     BREEDING_AGE = 5
@@ -9,27 +8,4 @@ class Fox(animal.Animal):
     COLOR = (200,0,0)
 
     def __init__(self,pos,grid):
-        super().__init__(pos,grid,self.__class__)
-        self.hunger = 0
-
-    def move(self):
-        super().move()
-        self.hunger=self.hunger+1
-        self.hunt()
-        if self.hunger >= self.MAX_HUNGER:
-            self.die()
-            return
-
-        #Hunt or Move the Fox to an adjacent empty location
-        #increment age, hunger, die if too old, hungry, or crowded
-        #give birth if of age
-
-    def hunt(self):
-        rb=self.grid.getAdjacentAnimal(self.pos, ['R'])
-        if rb != None:
-            rb.die()
-            self.hunger = 0
-
-        #find a rabbit and kill it >;)
-
-    
+        super().__init__(pos,grid,['R'])
--- wolf.py
+++ wolf.py
@@ -1,6 +1,5 @@
-import random
-import animal
-class Wolf(animal.Animal):
+import predator
+class Wolf(predator.Predator):
     MAX_AGE = 14
     NAME = "W"
     BREEDING_AGE = 5
@@ -9,18 +8,4 @@ class Wolf(animal.Animal):
     COLOR = (0,0,200)
 
     def __init__(self,pos,grid):
-        super().__init__(pos,grid,self.__class__)
-        self.hunger = 0
-
-    def move(self):
-        super().move()
-        self.hunger=self.hunger+1
-        self.hunt()
-        if self.hunger >= self.MAX_HUNGER:
-            self.die()
-            return
-    def hunt(self):
-        fx=self.grid.getAdjacentAnimal(self.pos, ['F'])
-        if fx != None:
-            fx.die()
-            self.hunger = 0
+        super().__init__(pos,grid,['F'])
